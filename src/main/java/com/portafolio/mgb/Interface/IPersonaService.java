20a34822c8df2bd1c04397395492e2445f1ab235
Your Name <you@example.com>
2023-02-02 12:02:54 -0300

    back compia

---
+++ src/main/java/com/portafolio/mgb/Interface/IPersonaService.java
@@ -0,0 +1,26 @@
+package com.portafolio.mgb.Interface;
+
+import com.portafolio.mgb.Entity.Persona;
+import java.util.List;
+
+
+
+public interface IPersonaService {
+    
+    //traer persona
+    public List<Persona>getPersona();
+    
+    //guardar un objeto de tipo persona
+    
+    public void savePersona(Persona persona);
+    
+    //eliminar un usuario pero lo buscamos por id
+    
+    public void deletePersona(Long id);
+    
+    //buscar un persona por id
+    
+    public Persona findPersona(Long id);
+    
+    
+}
